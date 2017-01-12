f08a9c2a2e11e45f64e7be4110f6f7c560c90057
rubin83 <rubin83@gmail.com>
2017-01-12 23:29:02 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Takes a matrix(called inputMat) and calculates the inverse of that matrix. If the matrix already exist,
+## calls the solution from the cache
 
-## Write a short comment describing this function
+## cache function that sets and retrieves the value of the inputed matrix, as well as, sets and retrieves the value of the inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+# the function that retrieves the inverse if it exists. If doesn't, calculate and stores the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+inputMat <- matrix(c(1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 0, 1, 4, 2, 3), 4, 4)
+cache <- makeCacheMatrix(inputMat)
+cacheSolve(cache)
+
+# repeat to verify getting cached data
+cacheSolve(cache)
